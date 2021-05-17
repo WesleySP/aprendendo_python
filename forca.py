b6959464fa19a99d4fc3377ba25fff2cffe27b4b
Wesley Santana <santanaw49@gmail.com>
2021-05-16 22:11:43 -0300

    Continuação do curso

--- forca.py
+++ forca.py
@@ -1,9 +1,28 @@
-
-
 def jogar():
     print("********************************")
     print("**Bem vindo no jogo da Forca!!**")
     print("********************************")
+
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while(not acertou and not enforcou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim de jogo")
 
